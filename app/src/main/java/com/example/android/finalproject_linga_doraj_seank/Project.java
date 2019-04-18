87a47760c5bfe549d0965a708024a31756b45291
jwq199497 <jwq199497@gmail.com>
2019-04-18 17:51:55 -0400

    dora1

--- app/src/main/java/com/example/android/finalproject_linga_doraj_seank/Project.java
+++ app/src/main/java/com/example/android/finalproject_linga_doraj_seank/Project.java
@@ -1,5 +1,49 @@
 package com.example.android.finalproject_linga_doraj_seank;
 
 public class Project {
-    String name;
+    public int name;
+    public int level;
+    public int time;
+    public int rating;
+
+    public Project(int name, int level, int time) {
+        this.name = name;
+        this.level = level;
+        this.time = time;
+    }
+
+    public Project(){
+
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public String toString() {
+        return "Project name:" + name +
+                "\n level: " + level +
+                "\n time: " + time +
+                "\n rating: " + rating;
+    }
 }
