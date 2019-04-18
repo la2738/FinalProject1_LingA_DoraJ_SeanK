d570b1d341961d31ebdc3ae23162080343bb8e09
jwq199497 <jwq199497@gmail.com>
2019-04-18 17:19:07 -0400

    dora1

---
+++ app/src/main/java/com/example/android/finalproject_linga_doraj_seank/CommunityFragment.java
@@ -0,0 +1,18 @@
+package com.example.android.finalproject_linga_doraj_seank;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CommunityFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_community, container, false);
+    }
+}
