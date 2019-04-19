31be509a50df1407010d4307692291c1f95a46df
jwq199497 <jwq199497@gmail.com>
2019-04-19 00:23:37 -0400

    dora

--- app/src/main/java/com/example/android/finalproject_linga_doraj_seank/MainActivity.java
+++ app/src/main/java/com/example/android/finalproject_linga_doraj_seank/MainActivity.java
@@ -14,8 +14,6 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private List<Project> projects;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,12 +22,6 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SearchFragment()).commit();
-
-        initialData();
-        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new ProjectsAdapter(projects, this));
 
     }
 
@@ -58,13 +50,4 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    private void initialData(){
-        projects = new ArrayList<>();
-        projects.add(new Project(R.string.counter_top_name,R.string.level_a1, R.string.counter_top_time,R.string.rating_4, R.drawable.countertop));
-        projects.add(new Project(R.string.ispring_name, R.string.level_i3, R.string.ispring_time, R.string.rating_5,R.drawable.ispringwaterfilter));
-        projects.add(new Project(R.string.smart_bidet_name, R.string.level_i1, R.string.smart_bidet_time,R.string.rating_4, R.drawable.smartbidet));
-        projects.add(new Project(R.string.screen_iphone_name, R.string.level_b2, R.string.screen_iphone_time,R.string.rating_5, R.drawable.screeniphone));
-        projects.add(new Project(R.string.drying_rack_name, R.string.level_m1, R.string.drying_rack_time,R.string.rating_5, R.drawable.dryingrack));
-        projects.add(new Project(R.string.pencil_box_name, R.string.level_b1, R.string.pencil_box_time,R.string.rating_4, R.drawable.pencilbox));
-    }
 }
--- app/src/main/java/com/example/android/finalproject_linga_doraj_seank/SearchFragment.java
+++ app/src/main/java/com/example/android/finalproject_linga_doraj_seank/SearchFragment.java
@@ -4,16 +4,41 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchFragment extends Fragment {
+
+    private List<Project> projects;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+
+        initialData();
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new ProjectsAdapter(projects, getContext()));
+
+        return rootView;
+    }
+
+    private void initialData(){
+        projects = new ArrayList<>();
+        projects.add(new Project(R.string.counter_top_name,R.string.level_a1, R.string.counter_top_time,R.string.rating_4, R.drawable.countertop));
+        projects.add(new Project(R.string.ispring_name, R.string.level_i3, R.string.ispring_time, R.string.rating_5,R.drawable.ispringwaterfilter));
+        projects.add(new Project(R.string.smart_bidet_name, R.string.level_i1, R.string.smart_bidet_time,R.string.rating_4, R.drawable.smartbidet));
+        projects.add(new Project(R.string.screen_iphone_name, R.string.level_b2, R.string.screen_iphone_time,R.string.rating_5, R.drawable.screeniphone));
+        projects.add(new Project(R.string.drying_rack_name, R.string.level_m1, R.string.drying_rack_time,R.string.rating_5, R.drawable.dryingrack));
+        projects.add(new Project(R.string.pencil_box_name, R.string.level_b1, R.string.pencil_box_time,R.string.rating_4, R.drawable.pencilbox));
     }
 
 
