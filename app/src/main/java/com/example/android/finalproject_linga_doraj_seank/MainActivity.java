9d78bd0910c51a95e418d8701fab692bd319806b
jwq199497 <jwq199497@gmail.com>
2019-04-19 01:20:11 -0400

    dora

--- app/src/main/java/com/example/android/finalproject_linga_doraj_seank/MainActivity.java
+++ app/src/main/java/com/example/android/finalproject_linga_doraj_seank/MainActivity.java
@@ -5,12 +5,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
